4e89b2495abd165e2d17231a4499c43bff1ede5f
Alex Jironkin <alex.jironkin@gmail.com>
2022-04-24 17:20:46 +0100

    Added base source directory and skipped test

---
+++ tdd_bank_dojo/account.py
@@ -0,0 +1,5 @@
+"""Classes and functions related to an Account"""
+
+
+class Account:
+    """Account class for managing funds"""
---
+++ tdd_bank_dojo/tests/account_test.py
@@ -0,0 +1,9 @@
+"""Account related tests"""
+
+import pytest
+
+
+@pytest.skip("My first test")
+def test_deposit_ammount():
+    """"""
+    assert True == False
