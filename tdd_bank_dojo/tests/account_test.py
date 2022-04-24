a21817a404dad135c8d8c5afbcffa2cd5b13fb53
Alex Jironkin <alex.jironkin@gmail.com>
2022-04-24 18:15:50 +0100

    Updated skip syntax to skip specific test

--- tdd_bank_dojo/tests/account_test.py
+++ tdd_bank_dojo/tests/account_test.py
@@ -3,7 +3,7 @@
 import pytest
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test_deposit_ammount():
     """Test deposit amunt reflects in Account"""
     assert True == False  # noqa: E712
